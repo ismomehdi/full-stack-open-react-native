ffd064af06685a66af510cd4035982e3cbb90f7d
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-22 14:00:57 +0200

    ex 10.12

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,18 +1,10 @@
 import { useQuery } from "@apollo/client";
-import { useEffect } from "react";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = () => {
   const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: "cache-and-network",
   });
-
-  useEffect(() => {
-    if (data) {
-      console.log("Data received:", data);
-    }
-  }, [data]);
-
   return { repositories: data?.repositories, loading, refetch };
 };
 
