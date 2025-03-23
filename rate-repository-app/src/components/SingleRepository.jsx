a4fa1e1267d72b626cfaa95cd646e54f4288ca7f
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-23 11:11:05 +0200

    ex 10.20

--- rate-repository-app/src/components/SingleRepository.jsx
+++ rate-repository-app/src/components/SingleRepository.jsx
@@ -1,9 +1,11 @@
+import { format } from "date-fns";
 import * as Linking from "expo-linking";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import { useParams } from "react-router-native";
 import useRepository from "../hooks/useRepository";
 import theme from "../theme";
 import RepositoryItem from "./RepositoryItem";
+import CustomText from "./Text";
 
 const styles = StyleSheet.create({
   container: {
@@ -22,12 +24,72 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
   },
-  background: {
-    backgroundColor: theme.colors.appBar,
-    height: "100%",
-    width: "100%",
+  reviewTopContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+  },
+  reviewRatingCircle: {
+    width: 40,
+    height: 40,
+    borderRadius: 40 / 2,
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  reviewRatingText: {
+    color: theme.colors.primary,
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  reviewTopTextContainer: {
+    flex: 1,
+    flexShrink: 1,
+    gap: 2,
+  },
+  reviewContainer: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 12,
+    backgroundColor: "white",
+    gap: 10,
+  },
+  separator: {
+    height: 10,
+    borderBottomColor: theme.colors.gray,
+    borderBottomWidth: 1,
+    margin: 10,
   },
 });
+
+const RepositoryInfo = ({ repository, openGitHub }) => (
+  <View>
+    <RepositoryItem item={repository} />
+    <View style={styles.container}>
+      <Pressable style={styles.button} onPress={openGitHub}>
+        <Text style={styles.buttonText}>Open in GitHub</Text>
+      </Pressable>
+    </View>
+  </View>
+);
+
+const ReviewItem = ({ review }) => (
+  <View style={styles.reviewContainer}>
+    <View style={styles.reviewTopContainer}>
+      <View style={styles.reviewRatingCircle}>
+        <Text style={styles.reviewRatingText}>{review.rating}</Text>
+      </View>
+      <View style={styles.reviewTopTextContainer}>
+        <CustomText fontWeight="bold">{review.user.username}</CustomText>
+        <CustomText>{format(review.createdAt, "dd.MM.yyyy")}</CustomText>
+      </View>
+    </View>
+    <CustomText>{review.text}</CustomText>
+  </View>
+);
 
 const SingleRepository = () => {
   const { id } = useParams();
@@ -39,16 +101,15 @@ const SingleRepository = () => {
   };
 
   return (
-    <View style={styles.background}>
-      <View>
-        <RepositoryItem item={repository} />
-        <View style={styles.container}>
-          <Pressable style={styles.button} onPress={openGitHub}>
-            <Text style={styles.buttonText}>Open in GitHub</Text>
-          </Pressable>
-        </View>
-      </View>
-    </View>
+    <FlatList
+      data={repository.reviews.edges.map((edge) => edge.node)}
+      renderItem={({ item }) => <ReviewItem review={item} />}
+      keyExtractor={({ id }) => id}
+      ListHeaderComponent={() => (
+        <RepositoryInfo repository={repository} openGitHub={openGitHub} />
+      )}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+    />
   );
 };
 
